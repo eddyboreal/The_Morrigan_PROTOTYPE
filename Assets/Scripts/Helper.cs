443fe77b72be64315954fef94bde83ffa1ae1c79
Mikaël <mika.fr99@hotmail.com>
2021-04-07 01:38:20 +0200

    Character Integration and Basic Anims

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    [Range(0,1)]
+    public float vertical;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical", vertical);
+    }
+}
